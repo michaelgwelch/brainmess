2c18341c2fc4b17e8e9d72d122e52f326b9b2eff
Michael Welch <michaelgwelch@gmail.com>
2012-02-09 22:05:42 -0600

    Prove the JumpForward algorithm

--- src.net/BrainMessSimple/BrainMessSimple/Main.cs
+++ src.net/BrainMessSimple/BrainMessSimple/Main.cs
@@ -166,9 +166,47 @@ output #12 even if it’s zero
 		// Should be part of "program" class. It only uses that data.
 		private void JumpForward()
 		{
+			// Precondition: Program Counter is pointing to the instruction immediately following a '[' instruction
 			System.Diagnostics.Debug.Assert(program[programCounter-1] == '[');
 			int nestLevel = 1;
 			
+			// Invariant: The nestLevel tells us how
+			// deeply nested the program counter is. A nest level of 0 means we are outside of the current loop.
+			// A nest level of 1 means we are inside the current loop. Etc. By current loop we mean the one started
+			// by the instruction that caused this method to be called. Namely the one we check for in Debug.Assert 
+			// above. In addition, if an iteration causes the nest level to change, we know that the instruction
+			// that caused the change is directly to the left of the program counter.
+			
+			// Initialization: The Loop Invariant holds at prior to loop after initialization. The Debug.Assert
+			//                 checks to make sure that we are indeed inside the current loop and we initialize 
+			//                 nestLevel to 1 to indicate that. (Trivially the nestLevel changed and the program
+			//                 counter is to the right of the instruction that caused the change).
+			
+			// Maintenance (inductive step): Check that the loop invariant holds at the end of each iteration of loop.
+			//     We have 4 cases to test: 1) the program counter is now pointing at '[', 2) the program counter
+			//     is now pointing at ']', or 3) the program counter is pointing at something other than '[' or ']',
+			//     and finally 4) the program counter is passed the last instruction.
+			//     Assume nestLevel is currently n.
+			//     Case 1: We read a ']' instruction and therefore decrease the nestLevel by 1 and increment the 
+			//             program counter. Since we just moved outside of a loop we expect the nestLevel to be n-1
+			//             and this is indeed the case. In addition, the nest level just changed and the instruction
+			//             that caused it is just to the left of program counter.
+			//     Case 2: We read a '[' instruction and move the program counter forward 1. We have just entered
+			//             a new loop and expect that the nestLevel is now (n+1). And this is indeed the case because
+			//             the conditional increments nestLevel if we see a '['. In addition we know the program
+			//             counter is direclty to the right of an instruction that caused the nest level to change.
+			//     Case 3: We don't see an instruction related to a jump and therefore our nest level has not
+			//             changed and therefore the nestLevel is still n. Nest level didn't change and therefore
+			//             we can't say that the program counter is directly to the right of a jump instruction.
+			//     Case 4: Program Terminates due to programCounter larger than size of program.
+			//             The loop invariant still holds.
+			
+			// Termination: At beginning program counter is q. At each iteration the program counter is incremented
+			//     by 1. Therefore there are 2 cases: 1) The end of the program is reached without finding a match
+			//     and 2) a match is found. In the first case, we terminate with an exception. In the second case,
+			//     we find a match and the nestLevel goes to 0 and we exit the loop. And by the loop invariant
+			//     we know that since the nest level just changed we know the program counter is directly to the
+			//     right of the matching ']' (which satisfies our post condition).
 			while(nestLevel > 0)
 			{
 				var currentInstruction = program[programCounter];
@@ -177,6 +215,8 @@ output #12 even if it’s zero
 				programCounter++;
 			}
 			
+			// Postcondition: Program Counter is pointing to the instruction immediately following the matching
+			//                ']' instruction. Or we terminate the program due to index out of bounds exception.
 		}
 		
 		// This only "plays" around with program. Seems like it should be part of
