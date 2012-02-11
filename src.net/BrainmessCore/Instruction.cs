b9ac45e4a1f90a22704305990cdd54758399c97c
Michael Welch <michaelgwelch@gmail.com>
2012-02-11 17:50:44 -0600

    Fix formatting of BrainmessCore classes and incorporate guid changes performed by visual studio

--- src.net/BrainmessCore/Instruction.cs
+++ src.net/BrainmessCore/Instruction.cs
@@ -3,73 +3,73 @@ using System.IO;
 
 namespace Welch.Brainmess
 {
-	public class Instruction
-	{
-		private readonly Action<ProgramStream, Tape, TextReader, TextWriter> _action;
-		private Instruction (Action<ProgramStream, Tape, TextReader, TextWriter> action)
-		{
-			_action = action;
-		}
-		
-		public void Execute(ProgramStream program, Tape tape, TextReader input, TextWriter output)
-		{
-			_action(program, tape, input, output);
-		}
-		
-		public static readonly Instruction MoveForward = 
-			new Instruction((program, tape, input, output) => tape.MoveForward());
-		
-		public static readonly Instruction MoveBackward = 
-			new Instruction((program, tape, input, output) => tape.MoveBackward());
-		
-		public static readonly Instruction Increment =
-			new Instruction((program, tape, input, output) => tape.Increment());
-		
-		public static readonly Instruction Decrement =
-			new Instruction((program, tape, input, output) => tape.Decrement());
-		
-		public static readonly Instruction Input =
-			new Instruction((program, tape, input, output) => tape.Current = input.Read());
-		
-		public static readonly Instruction Output =
-			new Instruction((program, tape, input, output) => output.Write((char)tape.Current));
-		
-		public static readonly Instruction TestAndJumpFoward =
-			new Instruction((program, tape, input, output) => {if (tape.Current == 0) program.JumpForward();});
+    public class Instruction
+    {
+        private readonly Action<ProgramStream, Tape, TextReader, TextWriter> _action;
+        private Instruction(Action<ProgramStream, Tape, TextReader, TextWriter> action)
+        {
+            _action = action;
+        }
 
-		public static readonly Instruction TestAndJumpBackward =
-			new Instruction((program, tape, input, output) => {if (tape.Current != 0) program.JumpBackward();});
-		
-		public static readonly Instruction NoOperation = 
-			new Instruction((program, tape, input, output) => {});
-		
-		public static Instruction FromInt(int characterValue)
-		{
-			switch(characterValue)
-			{
-			case '>':
-				return MoveForward;
-			case '<':
-				return MoveBackward;
-			case '+':
-				return Increment;
-			case '-':
-				return Decrement;
-			case '.':
-				return Output;
-			case ',':
-				return Input;
-			case '[':
-				return TestAndJumpFoward;
-			case ']':
-				return TestAndJumpBackward;
-			default:
-				return NoOperation;
-			}
-		}
-		                                                          
-	}
-	
-	
+        public void Execute(ProgramStream program, Tape tape, TextReader input, TextWriter output)
+        {
+            _action(program, tape, input, output);
+        }
+
+        public static readonly Instruction MoveForward =
+            new Instruction((program, tape, input, output) => tape.MoveForward());
+
+        public static readonly Instruction MoveBackward =
+            new Instruction((program, tape, input, output) => tape.MoveBackward());
+
+        public static readonly Instruction Increment =
+            new Instruction((program, tape, input, output) => tape.Increment());
+
+        public static readonly Instruction Decrement =
+            new Instruction((program, tape, input, output) => tape.Decrement());
+
+        public static readonly Instruction Input =
+            new Instruction((program, tape, input, output) => tape.Current = input.Read());
+
+        public static readonly Instruction Output =
+            new Instruction((program, tape, input, output) => output.Write((char)tape.Current));
+
+        public static readonly Instruction TestAndJumpFoward =
+            new Instruction((program, tape, input, output) => { if (tape.Current == 0) program.JumpForward(); });
+
+        public static readonly Instruction TestAndJumpBackward =
+            new Instruction((program, tape, input, output) => { if (tape.Current != 0) program.JumpBackward(); });
+
+        public static readonly Instruction NoOperation =
+            new Instruction((program, tape, input, output) => { });
+
+        public static Instruction FromInt(int characterValue)
+        {
+            switch (characterValue)
+            {
+                case '>':
+                    return MoveForward;
+                case '<':
+                    return MoveBackward;
+                case '+':
+                    return Increment;
+                case '-':
+                    return Decrement;
+                case '.':
+                    return Output;
+                case ',':
+                    return Input;
+                case '[':
+                    return TestAndJumpFoward;
+                case ']':
+                    return TestAndJumpBackward;
+                default:
+                    return NoOperation;
+            }
+        }
+
+    }
+
+
 }
 
--- src.net/BrainmessCore/Interpreter.cs
+++ src.net/BrainmessCore/Interpreter.cs
@@ -3,43 +3,43 @@ using System.IO;
 
 namespace Welch.Brainmess
 {
-	public class Interpreter
-	{
-		public Interpreter (ProgramStream program, Tape tape, TextReader input, TextWriter output)
-		{
-			_program = program;
-			_tape = tape;
-			_input = input;
-			_output = output;
-		}
-		
-		private ProgramStream _program;
-		
-		private Tape _tape;
-		
+    public class Interpreter
+    {
+        public Interpreter(ProgramStream program, Tape tape, TextReader input, TextWriter output)
+        {
+            _program = program;
+            _tape = tape;
+            _input = input;
+            _output = output;
+        }
 
-		
-		TextWriter _output;
-		TextReader _input;
+        private ProgramStream _program;
+
+        private Tape _tape;
 
 
-		
-		// uses "program" data, "tape" data, has information about instructions. Seems
-		// like an interaction between instructions, tape, and program.
-		
-		// Refactorings you can use. Extract method on each case body.
-		
-		// Unit testing this requires that you write "programs" to exercise all possibilities.
-		// Which is probably doable but tricky. 
-		public void Run()
-		{
-			while(!_program.EndOfProgram)
-			{
-				Instruction currentInstruction = _program.Fetch();
-				currentInstruction.Execute(_program, _tape, _input, _output);
-			}
-			
-		}
-	}
+
+        TextWriter _output;
+        TextReader _input;
+
+
+
+        // uses "program" data, "tape" data, has information about instructions. Seems
+        // like an interaction between instructions, tape, and program.
+
+        // Refactorings you can use. Extract method on each case body.
+
+        // Unit testing this requires that you write "programs" to exercise all possibilities.
+        // Which is probably doable but tricky. 
+        public void Run()
+        {
+            while (!_program.EndOfProgram)
+            {
+                Instruction currentInstruction = _program.Fetch();
+                currentInstruction.Execute(_program, _tape, _input, _output);
+            }
+
+        }
+    }
 }
 
