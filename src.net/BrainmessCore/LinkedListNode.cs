a108080880dcaa970a61ccadc80cc32e5a6b686e
Michael Welch <michaelgwelch@gmail.com>
2012-02-14 20:10:59 -0600

    Add XML documentation to LinkedListNOde

--- src.net/BrainmessCore/LinkedListNode.cs
+++ src.net/BrainmessCore/LinkedListNode.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 
 namespace Welch.Brainmess
 {
+    /// <summary>
+    /// Extendsion methods for LinkedListNode class
+    /// </summary>
     public static class LinkedListNode
     {
+        /// <summary>
+        /// Returns the next node after this one. If this is the last node in the list, then
+        /// a new node is added after this one (with the default value of T) and that node is returned.
+        /// If <paramref name="node"/> is not in a list then an exception is thrown.
+        /// </summary>
         public static LinkedListNode<T> MoveForward<T>(this LinkedListNode<T> node)
         {
             if (node.List == null) throw new ArgumentException("The specified node is not linked into a list", "node");
             return node.Next ?? node.List.AddLast(default(T));
         }
 
+        /// <summary>
+        /// Returns the node that is before this one. If this node is the first one in the list,
+        /// then a new node is added to the front of the list (with the default value of T) and that node
+        /// is returned. If <paramref name="node"/> is not part of a list then an exception is thrown.
+        /// </summary>
         public static LinkedListNode<T> MoveBackward<T>(this LinkedListNode<T> node)
         {
             if (node.List == null) throw new ArgumentException("The specified node is not linked into a list", "node");
