291c9d64ffe3a078d936ae302833b286d7413327
Michael Welch <michaelgwelch@gmail.com>
2012-02-18 22:47:28 -0600

    Convert from using [Int] to Seq Int for the tape.

--- haskell/tape.hs
+++ haskell/tape.hs
@@ -1,34 +1,30 @@
 module Tape where
 
+import Data.Sequence
 -- naive approach that just uses a regular list.
 
-data ListTraveler = Iterate [Int] Int deriving (Show, Eq)
+data ListTraveler = Iterate (Seq Int) Int deriving (Show, Eq)
 
 traveler :: ListTraveler
-traveler = Iterate [0] 0
+traveler = Iterate (singleton 0) 0
 
 moveNext :: ListTraveler -> ListTraveler 
-moveNext (Iterate xs pos) | pos >= ((length xs) - 1) = (Iterate (xs ++ [0]) (pos + 1))
+moveNext (Iterate xs pos) | pos >= ((Data.Sequence.length xs) - 1) = (Iterate (xs |> 0) (pos + 1))
                           | otherwise = Iterate xs (pos + 1)
 
 movePrev :: ListTraveler -> ListTraveler 
-movePrev (Iterate xs pos) | pos == 0 = Iterate (0 : xs) 0
+movePrev (Iterate xs pos) | pos == 0 = Iterate (0 <| xs) 0
                           | otherwise = Iterate xs (pos - 1)
 
 getCurrent :: ListTraveler -> Int
-getCurrent (Iterate xs pos) = xs !! pos
+getCurrent (Iterate xs pos) = index xs pos
 
 setCurrent :: ListTraveler -> Int -> ListTraveler
-setCurrent (Iterate xs pos) val | pos == 0 = Iterate (val : tail xs) pos
-                                | otherwise = 
-                                    Iterate (prefix ++ [val] ++ suffix) pos
-                                  where prefix = take pos xs
-                                        suffix = drop (pos+1) xs
-
+setCurrent (Iterate xs pos) val = (Iterate (update pos val xs) pos)
 
 data Tape = Tape ListTraveler deriving (Show, Eq)
 
-createTape :: [Int] -> Int -> Tape
+createTape :: Seq Int -> Int -> Tape
 createTape is pos = Tape (Iterate is pos)
 tape :: Tape
 tape = Tape traveler
