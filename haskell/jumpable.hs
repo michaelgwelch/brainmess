d7e12f6e56ae420eba1ea555b7e5c14774ac9537
Michael Welch <michaelgwelch@gmail.com>
2012-02-20 09:25:59 -0600

    Add documenation to jumpable

--- haskell/jumpable.hs
+++ haskell/jumpable.hs
@@ -1,5 +1,11 @@
--- ! Defines methods for jumping forward and backward on a data structure.
+-- | Defines methods for jumping forward and backward on types that 
+--   keep track of a current state.
 module Jumpable where
+
+-- | Defines jump methods for a data structure that maintains a state.
 class Jumpable a where
+    -- | Jumps the state forward
     jumpF :: a -> a
+
+    -- | Jumps the state backward
     jumpB :: a -> a
