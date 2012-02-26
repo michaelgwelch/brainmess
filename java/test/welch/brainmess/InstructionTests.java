2342863e5b01fd6a9cdf39865b4d5815f287fda1
Michael Welch <michaelgwelch@gmail.com>
2012-02-25 22:21:59 -0600

    Define IExecutionContext interface. Write some more tests

---
+++ java/test/welch/brainmess/InstructionTests.java
@@ -0,0 +1,66 @@
+package welch.brainmess;
+
+
+import org.junit.Test;
+import org.jmock.*;
+
+public class InstructionTests {
+
+	@Test
+	public void moveForward_Execute() {
+
+		// Arrange
+		Mockery mock = new Mockery();
+		final IExecutionContext context = mock.mock(IExecutionContext.class);
+		
+		
+		mock.checking(new Expectations() {{
+			oneOf(context).moveForward();
+		}});
+		
+		
+		// Act
+		Instruction.MoveForward.execute(context);
+		
+		// Assert
+		mock.assertIsSatisfied();
+	}
+
+
+	@Test
+	public void moveBackward_Execute() {
+
+		// Arrange
+		Mockery mock = new Mockery();
+		final IExecutionContext context = mock.mock(IExecutionContext.class);
+		
+		
+		mock.checking(new Expectations() {{
+			oneOf(context).moveBackward();
+		}});
+		
+		
+		// Act
+		Instruction.MoveBackward.execute(context);
+		
+		// Assert
+		mock.assertIsSatisfied();
+	}
+	
+	@Test
+	public void increment_Execute() {
+		// Arrange
+		Mockery mock = new Mockery();
+		final IExecutionContext context = mock.mock(IExecutionContext.class);
+		
+		mock.checking(new Expectations() {{
+			oneOf(context).testAndJumpForward();
+		}});
+		
+		// Act
+		Instruction.TestAndJumpForward.execute(context);
+		
+		// Assert
+		mock.assertIsSatisfied();
+	}
+}
