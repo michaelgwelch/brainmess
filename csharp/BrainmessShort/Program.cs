a023244f652d8421b3ce8c8c26562619b8b27fb1
Michael Welch <michaelgwelch@gmail.com>
2012-03-21 20:58:35 -0500

    Extract out class

---
+++ csharp/BrainmessShort/Program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace BrainmessShort
+{
+    public class Program
+    {
+        private readonly string program;
+        private int pc;
+        
+        public Program(string program)
+        {
+            this.program = program;
+            pc = 0;
+        }
+        
+        public bool EndOfProgram
+        {
+            get
+            {
+                return pc >= program.Length;
+            }
+        }
+        
+        public char Fetch()
+        {
+            var instruction = program[pc];
+            pc++;
+            return instruction;
+        }
+
+        public void JumpForward()
+        {
+            pc = program.FindMatch(pc - 1) + 1;
+        }
+
+        public void JumpBackward()
+        {
+            pc = program.FindMatch(pc - 1);
+        }
+    }
+}
+
