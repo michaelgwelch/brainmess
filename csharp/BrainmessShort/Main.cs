c75cc6a8cefaca42c66570f7bf49f6e16846dea6
Michael Welch <michaelgwelch@gmail.com>
2012-03-22 20:49:39 -0500

    Extract out tape class in BrainmessShort

--- csharp/BrainmessShort/Main.cs
+++ csharp/BrainmessShort/Main.cs
@@ -6,12 +6,12 @@ namespace BrainmessShort
    public class Brainmess
    {
         private readonly Program _program;
-        
-        private readonly int[] tape = new int[5000];
-        private int tc = 2500;
+        private readonly Tape _tape;
+
         public Brainmess(string programString) 
         {
             _program = new Program(programString);
+            _tape = new Tape();
         }
         
         public static void Main(string[] args)
@@ -21,37 +21,7 @@ namespace BrainmessShort
             reader.Close();
         }
 
-        int MoveForward()
-        {
-            return tc++;
-        }
 
-        int MoveBackward()
-        {
-            return tc--;
-        }
-
-        int Increment()
-        {
-            return tape[tc]++;
-        }
-
-        int Decrement()
-        {
-            return tape[tc]--;
-        }
-
-        int Current
-        {
-            get
-            {
-                return tape[tc];
-            }
-            set
-            {
-                tape[tc] = value;
-            }
-        }
         
         public void Run() 
         {
@@ -61,31 +31,31 @@ namespace BrainmessShort
                 switch(instruction)
                 {
                 case '>': 
-                    MoveForward();
+                    _tape.MoveForward();
                     break;
                 case '<':
-                    MoveBackward();
+                    _tape.MoveBackward();
                     break;
                 case '+':
-                    Increment();
+                    _tape.Increment();
                     break;
                 case '-':
-                    Decrement();
+                    _tape.Decrement();
                     break;
                 case '.':
-                    Console.Write((char)Current);
+                    Console.Write((char)_tape.Current);
                     break;
                 case ',':
-                    Current = Console.Read();
+                    _tape.Current = Console.Read();
                     break;
                 case '[':
-                    if (Current == 0)
+                    if (_tape.Current == 0)
                     {
                         _program.JumpForward();
                     }
                         break;
                 case ']':
-                    if (Current != 0)
+                    if (_tape.Current != 0)
                     {
                         _program.JumpBackward();
                     }
