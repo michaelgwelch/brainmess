76381bd24640c6f0291903d9a8ad213a50b9b5a2
Jmaxxz <jmaxxz@jmaxxz.com>
2012-04-09 18:38:09 -0500

    Pull all references to il out of main

--- brainmess-dotnet/bmc/BrainmessIlGenerator.cs
+++ brainmess-dotnet/bmc/BrainmessIlGenerator.cs
@@ -9,16 +9,38 @@ namespace Bmc
     public class BrainmessIlGenerator
     {
         private ILGenerator _ilg;
+        private AssemblyBuilder _ab;
+        private TypeBuilder _tb;
+        private MethodBuilder _fb;
+
         private Loop[] _loops;
-        private int nextUnusedLoopIndex =0;
-        public BrainmessIlGenerator(ILGenerator ilg, int loopCount, int tapeLength)
+        private int _nextUnusedLoopIndex =0;
+        private string _outputFilePath;
+        public BrainmessIlGenerator(string ouputFilePath, int loopCount, int tapeLength)
         {
-            _ilg =ilg;
+            _outputFilePath = ouputFilePath;
             _loops = new Loop[loopCount];
             InitializeProgram(tapeLength);
         }
         private void InitializeProgram(int tapeLength)
         {
+            AssemblyName an = new AssemblyName();
+            an.Name = "brainmess";
+            AppDomain ad = AppDomain.CurrentDomain;
+            _ab = ad.DefineDynamicAssembly(an, AssemblyBuilderAccess.Save);
+
+            var outputFile = new FileInfo(_outputFilePath);
+            ModuleBuilder mb = _ab.DefineDynamicModule(an.Name, outputFile.Name);
+
+            _tb = mb.DefineType("CompiledBrainmess",
+            TypeAttributes.Public|TypeAttributes.Class);
+
+
+            _fb = _tb.DefineMethod("Main",
+            MethodAttributes.Public|
+            MethodAttributes.Static, typeof(int), new Type[] { typeof(string[]) });
+            _ilg = _fb.GetILGenerator();
+
             //no program counter is needed as clr has that built in
             //initialize int array (tapeLength) in size
             LocalBuilder tapeVar = _ilg.DeclareLocal(typeof(int[]));//index 0 local var
@@ -43,6 +65,15 @@ namespace Bmc
         {
             _ilg.Emit(OpCodes.Ldc_I4_0);
             _ilg.Emit(OpCodes.Ret);//return 0
+
+            // Time to finish writing the type
+            _tb.CreateType();
+            // Set the entrypoint (thereby declaring it an EXE)
+            _ab.SetEntryPoint(_fb,PEFileKinds.ConsoleApplication);
+
+
+            // Save it
+            _ab.Save(_outputFilePath);
         }
 
         public void MoveTape(int x)
@@ -99,18 +130,18 @@ namespace Bmc
 
         public void BeginLoop()
         {
-            _ilg.MarkLabel(_loops[nextUnusedLoopIndex].LoopStart);
+            _ilg.MarkLabel(_loops[_nextUnusedLoopIndex].LoopStart);
             _ilg.Emit(OpCodes.Ldloc_S, 0);
             _ilg.Emit(OpCodes.Ldloc_S, 1);
             _ilg.Emit(OpCodes.Ldelem_I4);
             _ilg.Emit(OpCodes.Ldc_I4_S, 0);
-            _ilg.Emit(OpCodes.Beq,_loops[nextUnusedLoopIndex].LoopEnd);//if the current tape value == 0 jump past loop end
+            _ilg.Emit(OpCodes.Beq,_loops[_nextUnusedLoopIndex].LoopEnd);//if the current tape value == 0 jump past loop end
 
-            nextUnusedLoopIndex++;
+            _nextUnusedLoopIndex++;
         }
         public void EndLoop()
         {
-            Loop loopBeingClosed = _loops.Take(nextUnusedLoopIndex).Last(x=>!x.HasBeenClosed);
+            Loop loopBeingClosed = _loops.Take(_nextUnusedLoopIndex).Last(x=>!x.HasBeenClosed);
             loopBeingClosed.HasBeenClosed = true;
             _ilg.Emit(OpCodes.Br,loopBeingClosed.LoopStart);
             _ilg.MarkLabel(loopBeingClosed.LoopEnd);
